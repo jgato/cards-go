a1562459f4dd013ded0e0446c572af8b9076c29b
Jose Gato <jgato@redhat.com>
2022-01-16 17:40:50 +0100

    some more fun
    allows to pass a file full of urls separated by \n
    allows to parallelize the links check

--- concurrence/main.go
+++ concurrence/main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 )
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+// thanks to https://stackoverflow.com/a/18479916/15383067
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
 
 func main() {
 	links := []string{
@@ -15,9 +35,20 @@ func main() {
 		"http://redhat.com",
 	}
 
-	if len(os.Args) > 1 && os.Args[1] == "--parallel" {
+	// dont really make good checks of params, but.. just playing
+	// main.go [file_with_urls] [--parallel NUMCPUS]
+	if len(os.Args) > 1 {
+		linksFromFile, err := readLines(os.Args[1])
+		if err != nil {
+			fmt.Println("error opening file")
+			os.Exit(1)
+		}
+		links = linksFromFile
+	}
+	fmt.Println("ok lets go")
+	if len(os.Args) > 2 && os.Args[2] == "--parallel" { // I trust you also will give the num of cpus
 		c := make(chan string)
-		NUMCPUS := 2
+		NUMCPUS, _ := strconv.Atoi(os.Args[3])
 		init := 0
 		end := NUMCPUS
 		iterations := 1
